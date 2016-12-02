358dbc5bd30f4f5ca85dc8024358d31dba6cad7e
Stefan Hartmann <stefanhartmann@gmail.com>
2016-12-02 14:50:42 -0500

    remove shitty nav items

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -8,18 +8,6 @@ export default class Navbar extends Component {
           <nav className="nav-items">
             <img id="paw-icon" src="./style/icons/paw-icon.png"/>
             <h1 className="brand-logo center"><span className="small-logo">pe</span>tinder</h1>
-            <ul className="right">
-              <li>
-                <a href="#">
-                  myPets
-                </a>
-              </li>
-              <li>
-                <a href="#">
-                  About
-                </a>
-              </li>
-            </ul>
           </nav>
       </div>
     </div>
